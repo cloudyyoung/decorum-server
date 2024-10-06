c206dc2d97346980c37d503011b7747e01564a68
Yunfan Yang <cloudy.young@outlook.com>
2024-10-05 18:39:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import randint, seed
+from random import seed, choices
+import string
 from time import time
 from fastapi import FastAPI
 from decorum_generator import GameGenerator
@@ -15,13 +16,17 @@ async def root():
 
 @app.post("/game")
 async def game(game: Game):
+    seed(None)
+
     if not game.seed:
-        game.seed = str(time())
+        game.seed = "".join(choices(string.ascii_letters + string.digits, k=4)).upper()
 
     seed(game.seed)
 
     game_generator = GameGenerator(game.num_of_players, game.total_difficulty_points)
     game_generator.generate_conditions()
     conditions = game_generator.pick_conditions()
+
+    seed(None)
 
     return {"game": game}
